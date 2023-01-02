1e39a53df48c35424f657b94784f45f1eea2781d
Sandali Disara <sandalidissanayake123@gmail.com>
2023-01-02 22:37:58 +0530

    Create Image Cards on Home Page

---
+++ react-website-photo/src/components/Cards.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import CardItem from './CardItem';
+import './Cards.css';
+
+function Cards() {
+    return (
+        <div className='cards'>
+            <h1>Check out these pictures</h1>
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
+                        <CardItem
+                            src="images/img01.jpg"
+                            text="Explore the waterfall in the amazon"
+                            label="Adventure"
+                            path="/services" />
+                        <CardItem
+                            src="images/img01.jpg"
+                            text="Explore the waterfall in the amazon"
+                            label="Adventure"
+                            path="/services" />
+
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
