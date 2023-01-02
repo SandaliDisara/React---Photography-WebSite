e93e806fb6bc70a25a638a9c4f30a27db60fb0d2
Sandali Disara <sandalidissanayake123@gmail.com>
2023-01-02 22:18:30 +0530

    Create HeroSection of Home page

--- react-website-photo/src/App.js
+++ react-website-photo/src/App.js
@@ -1,20 +1,25 @@
-import './App.css';
+import React from 'react';
 import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 
 function App() {
   return (
-
-    <Router>
-      <div className='App'>
-        <Routes>
-          <Route path='/' element={<Navbar />} />
-        </Routes>
-      </div>
-
-    </Router>
-
-
+    
+      <Router>
+        <div className='App'>
+          <Routes>
+         
+        
+          <Route path='/' element={<><Navbar/><Home/></>} />
+          
+          
+          </Routes>
+        </div>
+      </Router>
+    
   );
 }
 
