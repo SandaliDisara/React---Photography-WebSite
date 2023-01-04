427d055531af9fdfe819958506cf3bdd83a6a46b
Sandali Disara <sandalidissanayake123@gmail.com>
2023-01-04 14:44:32 +0530

    Add the images to page

--- react-website-photo/src/components/pages/photos.js
+++ react-website-photo/src/components/pages/photos.js
@@ -6,16 +6,49 @@ function Photos() {
     return (
         <section>
             <div class="container">
-                <img src="images/img01.jpg" />
-                <img src="images/img01.jpg" />
-                <img src="images/img01.jpg" />
-                <img src="images/img01.jpg" />
-                <img src="images/img01.jpg" />
-                <img src="images/img01.jpg" />
-                <img src="images/img01.jpg" />
-                <img src="images/img01.jpg" />
-                <img src="images/img01.jpg" />
-                <img src="images/img01.jpg" />
+                <img src="images/img39.jpg" />
+
+                <img src="images/img38.jpg" />
+                <img src="images/img37.jpg" />
+                <img src="images/img36.jpg" />
+                <img src="images/img35.jpg" />
+                <img src="images/img34.jpg" />
+
+                <img src="images/img33.jpg" />
+                <img src="images/img31.jpg" />
+                <img src="images/img30.jpg" />
+                <img src="images/img29.jpg" />
+                <img src="images/img27.jpg" />
+
+                <img src="images/img26.jpg" />
+                <img src="images/img25.jpg" />
+                <img src="images/img24.jpg" />
+                <img src="images/img23.jpg" />
+                <img src="images/img22.jpg" />
+
+                <img src="images/img21.jpg" />
+                <img src="images/img20.jpg" />
+                <img src="images/img19.jpg" />
+                <img src="images/img18.jpg" />
+                <img src="images/img17.jpg" />
+
+                <img src="images/img16.jpg" />
+                <img src="images/img15.jpg" />
+                <img src="images/img14.jpg" />
+                <img src="images/img13.jpg" />
+                <img src="images/img12.jpg" />
+
+                <img src="images/img11.jpg" />
+                <img src="images/img10.jpg" />
+                <img src="images/img09.jpg" />
+                <img src="images/img08.jpg" />
+                <img src="images/img07.jpg" />
+
+                <img src="images/img06.jpg" />
+                <img src="images/img05.jpg" />
+                <img src="images/img04.jpg" />
+                <img src="images/img03.jpg" />
+                <img src="images/img02.jpg" />
             </div>
 
         </section>
